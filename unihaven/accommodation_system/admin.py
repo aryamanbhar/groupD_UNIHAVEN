77ef8351901d8e096493bcf5d7d332f9e230e615
Khan Saahil Alam <saahilalam@KEUNG-TO.local>
2025-04-02 20:23:11 +0800

    Registered User and Contract models

--- unihaven/accommodation_system/admin.py
+++ unihaven/accommodation_system/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import *
+
+admin.site.register(User)
 admin.site.register(Accommodation)
 admin.site.register(Reservation)
 admin.site.register(Student)
 admin.site.register(Rating)
 admin.site.register(CedarsSpecialist)
 admin.site.register(Notification)
+admin.site.register(Contract)
+
