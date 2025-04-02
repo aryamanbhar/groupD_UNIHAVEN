93d3bf8c40ededb9a9763bd6c824d03bf6ee330e
SaahilAlam1 <136902134+SaahilAlam1@users.noreply.github.com>
2025-04-02 21:53:23 +0800

    Update serializers.py

---
+++ unihaven/accommodation_system/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from .models import (
+    User, CedarsSpecialist, Accommodation, Student, Reservation,
+    Contract, Rating, Notification
+)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "phone", "created_at"]
+
+class CedarsSpecialistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CedarsSpecialist
+        fields = ["id", "department"]
+
+class AccommodationSerializer(serializers.ModelSerializer):
+    cedars_specialist = CedarsSpecialistSerializer()
+
+    class Meta:
+        model = Accommodation
+        fields = [
+            "id", "name", "image", "type", "owner_info", "longitude", "latitude",
+            "area", "distance", "price", "number_of_beds", "number_of_bedrooms",
+            "availability_start", "availability_end", "create_date", "status",
+            "cedars_specialist"
+        ]
+
+class StudentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Student
+        fields = ["id", "user", "name", "degree_type"]
+
+class ReservationSerializer(serializers.ModelSerializer):
+    student = StudentSerializer()
+    accommodation = AccommodationSerializer()
+
+    class Meta:
+        model = Reservation
+        fields = ["id", "student", "accommodation", "status"]
+
+class ContractSerializer(serializers.ModelSerializer):
+    reservation = ReservationSerializer()
+
+    class Meta:
+        model = Contract
+        fields = ["id", "reservation", "date", "document", "signed_at", "contract_status"]
+
+class RatingSerializer(serializers.ModelSerializer):
+    student = StudentSerializer()
+    accommodation = AccommodationSerializer()
+
+    class Meta:
+        model = Rating
+        fields = ["id", "student", "accommodation", "score"]
+
+class NotificationSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    cedars_specialist = CedarsSpecialistSerializer()
+
+    class Meta:
+        model = Notification
+        fields = ["id", "user", "detail", "is_read", "cedars_specialist"]
