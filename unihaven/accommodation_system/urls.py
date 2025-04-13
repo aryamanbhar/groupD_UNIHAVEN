aeee13c54529591779e7114fddc3b50aa640ac2f
Khan Saahil Alam <saahilalam@KEUNG-TO.local>
2025-04-14 01:55:34 +0800

    added reservation endpoints

--- unihaven/accommodation_system/urls.py
+++ unihaven/accommodation_system/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
 from .views2 import (
- AccommodationDetail, AccommodationSearch, AccommodationUpload, AccommodationRetrieveUpdateDeleteView, ReservationViewAll, ReservationDetailView
+ AccommodationDetail, AccommodationSearch, AccommodationUpload, AccommodationRetrieveUpdateDeleteView, ReservationCancelView, 
+ ReservationCreateView, ReservationListView, ReservationViewAll, ReservationDetailView
 )
   
 urlpatterns = [
-    # Handles /accommodations/{search}
     path('accommodations/upload/', AccommodationUpload.as_view(), name='accommodation-list'),  
     path('accommodations/search/', AccommodationSearch.as_view(), name='accommodation-search'),
     path('accommodations/search/<str:property_id>/', AccommodationDetail.as_view(), name='accommodation-detail'),
-
-    path('reservations/', ReservationViewAll.as_view(), name='reservation-list-create'),
-    path('reservations/<int:id>/', ReservationDetailView.as_view(), name='reservation-detail')
-
+    path("reservations/create/", ReservationCreateView.as_view(), name="reservation-create"),
+    path("reservations/<str:reservation_id>/cancel/", ReservationCancelView.as_view(), name="reservation-cancel"),
+    path("reservations/<str:reservation_id>/", ReservationDetailView.as_view(), name="reservation-detail"),
+    path("reservations/", ReservationListView.as_view(), name="reservation-list"),
 ]
