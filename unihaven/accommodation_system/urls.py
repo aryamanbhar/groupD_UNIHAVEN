64efdd50c9861c0033734ad24789be1631f60d83
g1105530 <yibo8034@gmail.com>
2025-04-16 19:36:30 +0800

    Update urls.py

--- unihaven/accommodation_system/urls.py
+++ unihaven/accommodation_system/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("students/create/", StudentCreateView.as_view(), name="student-create"),
     path("cedars_specialists/create/", CedarsSpecialistCreateView.as_view(), name="cedars-specialist-create"),
     path("cedars_specialists/", CedarsSpecialistListView.as_view(), name="cedars-specialist-list"),
+    path('accommodation-rate/', RatingCreateView.as_view(), name='accommodation-rate')，
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema')),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'))
