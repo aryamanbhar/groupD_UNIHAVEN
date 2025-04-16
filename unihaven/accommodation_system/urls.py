d2840532e4b0d369c5a03f19b6f6da61ac344a78
aryamanbhar <u3585875@connect.hku.hk>
2025-04-16 21:13:20 +0800

    .

--- unihaven/accommodation_system/urls.py
+++ unihaven/accommodation_system/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views2 import (
  AccommodationDetail, AccommodationSearch, AccommodationUpload, AccommodationsViewAll, AccommodationRetrieveUpdateDeleteView, ReservationCancelView, 
- ReservationCreateView, ReservationListView, ReservationDetailView, StudentCreateView, CedarsSpecialistCreateView, CedarsSpecialistListView
+ ReservationCreateView, ReservationListView, ReservationDetailView, StudentCreateView, CedarsSpecialistCreateView, CedarsSpecialistListView, RatingCreateView
 )
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
@@ -17,7 +17,7 @@ urlpatterns = [
     path("students/create/", StudentCreateView.as_view(), name="student-create"),
     path("cedars_specialists/create/", CedarsSpecialistCreateView.as_view(), name="cedars-specialist-create"),
     path("cedars_specialists/", CedarsSpecialistListView.as_view(), name="cedars-specialist-list"),
-    path('accommodation-rate/', RatingCreateView.as_view(), name='accommodation-rate'),
+    path('accommodations/rate/', RatingCreateView.as_view(), name='accommodation-rate'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema')),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'))
