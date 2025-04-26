130acc2adf7db4dfead6013dcb13742301c475e4
Mmaggiemw <159514914+Mmaggiemw@users.noreply.github.com>
2025-04-26 22:59:57 +0800

    Delete unihaven-extension directory

--- unihaven-extension/src/common/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    email = forms.EmailField(label='Email', max_length=255)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
