ad7b46493caac060520a59b6a3960cad04fbe071
Mmaggiemw <159514914+Mmaggiemw@users.noreply.github.com>
2025-04-26 22:45:42 +0800

    Delete unihaven-extension directory

--- unihaven-extension/src/apps/hku/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class HkuConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.hku'  # Ensure this matches the app's full Python path
