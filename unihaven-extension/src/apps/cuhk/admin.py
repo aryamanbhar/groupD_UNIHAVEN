57fd74d76d135576e34ca2062545e4977b84289b
Mmaggiemw <159514914+Mmaggiemw@users.noreply.github.com>
2025-04-27 03:04:59 +0800

    Add files via upload

---
+++ unihaven-extension/src/apps/cuhk/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(Accommodation)
+admin.site.register(Reservation)
+admin.site.register(Student)
+admin.site.register(Rating)
+admin.site.register(CedarsSpecialist)
+admin.site.register(Contract)
+
