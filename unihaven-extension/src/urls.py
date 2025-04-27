2478ac1594159f914c0667de50708ae8d18c0bb1
g1105530 <yibo8034@gmail.com>
2025-04-27 16:10:24 +0800

    Update urls.py

--- unihaven-extension/src/urls.py
+++ unihaven-extension/src/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('hkust/', include('apps.hkust.urls')),  # Ensure this line is correct
     path('login/', LoginView.as_view(), name='login'),  # Add the login URL here
     path('register/', RegisterView.as_view(), name='register'),
+    path('ratings/create/', views.RatingCreateView.as_view(), name='rating-create'),
+    path('ratings/', views.RatingListView.as_view(), name='rating-list'),
 ]
