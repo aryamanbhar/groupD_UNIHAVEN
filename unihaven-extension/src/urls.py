78b7a62f94d789e1d0bad16b7786be7a74a1a330
Khan Saahil Alam <saahilalam@KEUNG-TO.local>
2025-05-03 21:32:31 +0800

    url for api doc

--- unihaven-extension/src/urls.py
+++ unihaven-extension/src/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from django.contrib import admin
 from common.views import LoginView, RegisterView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('hku/', include('apps.hku.urls')),
     path('cuhk/', include('apps.cuhk.urls')),
-    path('hkust/', include('apps.hkust.urls')),  # Ensure this line is correct
-    path('login/', LoginView.as_view(), name='login'),  # Add the login URL here
-    path('register/', RegisterView.as_view(), name='register')
+    path('hkust/', include('apps.hkust.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
